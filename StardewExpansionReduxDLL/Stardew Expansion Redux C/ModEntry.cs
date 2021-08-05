8045ccc0d233ed6fd891224de7b60ed088bc8d3d
AnotherPillow <Thepilous_@outlook.com>
2021-08-05 10:14:49 +0100

    Uploaded code (code v1.0, main 1.5)

---
+++ StardewExpansionReduxDLL/Stardew Expansion Redux C/ModEntry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Objects;
+using PyTK.Extensions;
+using PyTK.Types;
+using Microsoft.Xna.Framework;
+
+namespace StardewExpansionReduxDLL
+{
+    public class ModEntry : Mod
+    {
+        //This is what the ModEntry is for, to be the entry to your mod ^^
+        //So here should be (only) the entry method and events you add in the entry method
+        public override void Entry(IModHelper helper)
+        {
+            new TileAction("TSVFixMinecart1", FixMinecart.FixMinecart1).register();
+        
+        }
+    }
+    class FixMinecart
+    {
+
+        internal static void TSVFixMinecart1()
+        {
+            if (Game1.player.hasOrWillReceiveMail("8968.Minecart1"))
+            {
+                Game1.drawObjectDialogue("You already fixed this minecart");
+            }
+            else if (Game1.player.Money < 10000)
+            {
+                Game1.drawObjectDialogue("Not enough gold.");
+            }
+            else
+            {
+                Game1.player.Money -= 10000;
+                Game1.player.mailReceived.Add("8968.Minecart1");
+                Game1.drawObjectDialogue("Minecart is being fixed, please return soon.");
+            }
+        }
+
+        internal static bool FixMinecart1(string arg1, GameLocation arg2, Vector2 arg3, string arg4)
+        {
+            //Creating the list of possible answers with the functions that should be called when the answer left of them is being selected
+            List<QuestionAnswer> ltAnswers = new List<QuestionAnswer>()
+            {
+                new QuestionAnswer("Fix minecart Number 1 for 10000G", TSVFixMinecart1),
+                new QuestionAnswer("Don't fix minecart", null)
+            };
+
+            // Calls the QuestionDialogue and thus displays the question dialogue, with the given answers
+            new QuestionDialogue("This minecart seems broken, would you like to fix it for 10000G?", ltAnswers);
+
+            //why just return true? If its not needed delete the line and change the bool into void :D
+            return true;
+        }
+    }
+}
---
+++ StardewExpansionReduxDLL/Stardew Expansion Redux C/QuestionAnswer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewExpansionReduxDLL
+{
+    //One file for each class makes everything far more readable :D
+    // + QuestionAnswer shouldnt be in the class ModEntry cuz then youd always have to call ModEntry.QuestionAnswer when you use it elsewhere (i think)
+    internal class QuestionAnswer
+    {
+        // The answer key provided to the game.
+        public string sKey { get; }
+
+        // Handles the answer when the player chooses it.
+        public Action fuHandler { get; }
+
+        // Construct an instance.
+        // "sKey" The answer key provided to the game.
+        // "fuHandler" Handles the answer when the player chooses it.
+        public QuestionAnswer(string sKey, Action fuHandler)
+        {
+            this.sKey = sKey;
+            this.fuHandler = fuHandler;
+        }
+    }
+}
